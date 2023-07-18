624c774fb5759aaf24dff5c2c7fc4e7682e2fbca
aleli <alexredd95@gmail.com>
2023-07-18 19:37:31 -0300

    ++arreglados errores

--- src/controllers/turno.controller.js
+++ src/controllers/turno.controller.js
@@ -19,7 +19,6 @@ async function getOccupiedTurnos(req, res) {
   }
 }
 
-
 async function addTurno(req, res) {
   const { date, userId, doctorId } = req.body;
   try {
@@ -29,7 +28,7 @@ async function addTurno(req, res) {
 
     if (doctorCheck && date === doctorCheck.date) {
       console.log('The Doctor already has a turno');
-      return res.status(400).send('The Doctor already has a turno');
+      return res.status(401).send('The Doctor already has a turno');
     }
 
     if (pacienteHasTurno) {
@@ -45,30 +44,6 @@ async function addTurno(req, res) {
     res.status(500).send('Error creating turno');
   }
 }
-
-
-
-// const getTurnoWithUser = async (req, res) => {
-//   try {
-//     const turnoId = req.body.turnoId;
-//     const turno = await Turno.findByPk(turnoId, { include: 'paciente' });
-
-//     if (!turno) {
-//       console.log('Turno not found');
-//       res.status(404).send('Turno no encontrado')
-//       return;
-//     }
-
-//     const user = turno.paciente;
-
-//     console.log('Turno:', turno.toJSON());
-//     console.log('User:', user.toJSON());
-//     res.send('este es el turno con fecha y hora ' + turno.date + ' de ' + user.username)
-//   } catch (error) {
-//     console.error('Error retrieving turno:', error);
-//     res.status(400).send('Ha habido un error')
-//   }
-// };
 
 async function getPacienteTurno(req, res) {
   try {
