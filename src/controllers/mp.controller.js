0b4fc321c63c3edd17d3321d0fbfc7a5b7eeaf44
LuisLuna810 <luis.agustin10@hotmail.com>
2023-08-01 12:27:25 -0300

    subscription

--- src/controllers/mp.controller.js
+++ src/controllers/mp.controller.js
@@ -44,15 +44,65 @@ async function setPreferences(req, res) {
     .catch((error) => res.status(400).send({ error: error.message }));
 }
 
+
+//Esta función se encarga de crear el id de Referencia para que se genere el checkout de MP
+async function setPreferencesSubscription(req, res) {
+  const { user, price } = req.body;
+
+
+  let preference = {
+    items: [
+      {
+        title: "Subscripción",
+        unit_price: Number(price),
+        quantity: 1,
+        currency_id: "ARS",
+      },
+    ],
+    payer:{
+      id: user.id
+    }
+    ,
+    back_urls: {
+      success: `http://localhost:4000/feedbackSubscription`,
+      failure: `http://localhost:4000/feedbackSubscription`,
+      pending: "http://localhost:4000/feedbackSubscription",
+    },
+    binary_mode: true,
+    auto_return: "approved",
+    
+    }
+
+  mercadopago.preferences
+    .create(preference)
+    .then((response) => res.status(200).send({ response }))
+    .catch((error) => res.status(400).send({ error: error.message }));
+}
+
 async function feedback(req, res) {
-  res.json({
+  let dataPay = {
     Payment: req.query.payment_id,
     Status: req.query.status,
     MerchantOrder: req.query.merchant_order_id,
-  });
+  };
+  res.json(dataPay)
+}
+
+async function feedbackSubscription(req, res) {
+  let dataPay = {
+    Payment: req.query.payment_id,
+    Status: req.query.status,
+    MerchantOrder: req.query.merchant_order_id,
+    ExternalReference: req.query.external_reference,
+  };
+  res.json(dataPay)
 }
 
 module.exports = {
   setPreferences,
   feedback,
+  setPreferencesSubscription,
+  feedbackSubscription,
+
+
 };
--- src/routes/mp.routes.js
+++ src/routes/mp.routes.js
@@ -4,7 +4,9 @@ const mpController = require('../controllers/mp.controller');
 const jwtVerify = require('../middlewares/isAuth')
 
 router.post('/mpcheckout', /*jwtVerify*/ mpController.setPreferences)
+router.post('/mpcheckoutSubscription', /*jwtVerify*/ mpController.setPreferencesSubscription)
 router.get('/feedback', /*jwtVerify*/ mpController.feedback)
+router.get('/feedbackSubscription', /*jwtVerify*/ mpController.feedbackSubscription)
 
 
 module.exports = router;
